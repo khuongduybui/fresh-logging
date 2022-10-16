84acc66ef8db8cd64d4da2a4cdceb5b35ff6bfcb
Duy K. Bui <duy@buifamily.info>
2022-10-16 14:37:04 -0400

    refactor: the rise of SkyResolver

--- index.ts
+++ index.ts
@@ -37,6 +37,14 @@ const _defaultLogger = new Logger("fresh-logging-default-logger").addStream(
     ),
 );
 
+const _durationCalculator = async (callable: () => Promise<unknown>) => {
+  const start = performance.now();
+  const res = await callable();
+  const end = performance.now();
+  const duration = (end - start).toFixed(1);
+  return [duration, res];
+};
+
 /**
  * Get a Deno Fresh middleware that log the request / response in Common Log Format.
  *
@@ -44,75 +52,77 @@ const _defaultLogger = new Logger("fresh-logging-default-logger").addStream(
  * @returns A middleware that logs request / response in the specified format, or a no-op middleware.
  */
 export function getLogger(options?: LoggingOpts): MiddlewareHandler {
+  // Populate default values for options
   const format = options?.format ?? LoggingFormat.COMMON;
   const includeDuration = options?.includeDuration ?? true;
   const resolvers = options?.resolvers ?? {};
-  resolvers[ResolutionField.rfc931] = resolvers[ResolutionField.rfc931] ?? (() => "-");
-  resolvers[ResolutionField.authuser] = resolvers[ResolutionField.authuser] ?? (() => "-");
-  resolvers[ResolutionField.bytes] = resolvers[ResolutionField.bytes] ?? (() => "-");
   const logger = options?.logger ?? _defaultLogger.info.bind(_defaultLogger);
   const combinedHeaders = options?.combinedHeaders ?? ["Referer", "User-agent"];
 
+  // Resolvers
+  const noopResolver = ((_req: Request, _ctx: MiddlewareHandlerContext, _res: Response) => "-");
+  resolvers[ResolutionField.rfc931] = resolvers[ResolutionField.rfc931] ?? noopResolver;
+  resolvers[ResolutionField.authuser] = resolvers[ResolutionField.authuser] ?? noopResolver;
+  resolvers[ResolutionField.bytes] = resolvers[ResolutionField.bytes] ?? noopResolver;
+
+  const remoteIpResolver = (_req: Request, ctx: MiddlewareHandlerContext, _res: Response) => (ctx.remoteAddr as Deno.NetAddr).hostname;
+
+  const durationResolver = async (_req: Request, ctx: MiddlewareHandlerContext) => {
+    const [duration, res] = await _durationCalculator(() => ctx.next()) as [string, Response];
+    let durationText = "";
+    if (includeDuration) {
+      res.headers.set("Server-Timing", `handler;dur=${duration}`);
+      durationText = `${duration}ms`;
+    }
+    return [durationText, res];
+  };
+
+  const timestampResolver = (_req: Request, _ctx: MiddlewareHandlerContext, _res: Response) => {
+    const now = options?.utcTime ? DateTime.utc() : DateTime.now();
+    return `[${now.toFormat("dd/MMM/yyyy:HH:mm:ss ZZZ")}]`;
+  };
+
+  const methodResolver = (req: Request, _ctx: MiddlewareHandlerContext, _res: Response) => req.method;
+
+  const urlResolver = (req: Request, _ctx: MiddlewareHandlerContext, _res: Response) => req.url;
+
+  const statusResolver = (_req: Request, _ctx: MiddlewareHandlerContext, res: Response) => res.status;
+
+  const headerResolver = (req: Request, _ctx: MiddlewareHandlerContext, _res: Response, header: string) =>
+    req.headers.has(header) ? `"${req.headers.get(header)}"` : "-";
+
+  const commonLogPartsResolver = async (req: Request, ctx: MiddlewareHandlerContext) => {
+    const [durationText, res] = await durationResolver(req, ctx) as [string, Response];
+    const logParts = [
+      remoteIpResolver(req, ctx, res),
+      await resolvers[ResolutionField.rfc931]?.(req, ctx, res),
+      await resolvers[ResolutionField.authuser]?.(req, ctx, res),
+      timestampResolver(req, ctx, res),
+      methodResolver(req, ctx, res),
+      urlResolver(req, ctx, res),
+      statusResolver(req, ctx, res),
+      await resolvers[ResolutionField.bytes]?.(req, ctx, res),
+    ];
+    return [durationText, res, logParts];
+  };
+
+  // Building log entry according to format
   switch (format) {
     case LoggingFormat.COMMON:
-      return async (
-        req: Request,
-        ctx: MiddlewareHandlerContext,
-      ): Promise<Response> => {
-        const now = options?.utcTime ? DateTime.utc() : DateTime.now();
-
-        const start = performance.now();
-        const res = await ctx.next();
-        const end = performance.now();
-        const duration = (end - start).toFixed(1);
-        let durationText = "";
-        if (includeDuration) {
-          res.headers.set("Server-Timing", `handler;dur=${duration}`);
-          durationText = `${duration}ms`;
-        }
-        const logParts = [
-          (ctx.remoteAddr as Deno.NetAddr).hostname,
-          await resolvers[ResolutionField.rfc931]?.(req, ctx, res),
-          await resolvers[ResolutionField.authuser]?.(req, ctx, res),
-          `[${now.toFormat("dd/MMM/yyyy:HH:mm:ss ZZZ")}]`,
-          `"${req.method} ${req.url}"`,
-          res.status,
-          await resolvers[ResolutionField.bytes]?.(req, ctx, res),
-          durationText,
-        ];
+      return async (req: Request, ctx: MiddlewareHandlerContext): Promise<Response> => {
+        const [durationText, res, commonLogParts] = await commonLogPartsResolver(req, ctx) as [string, Response, string[]];
+        const logParts = commonLogParts
+          .concat([durationText]);
         logger(logParts.join(" "));
-
         return res;
       };
     case LoggingFormat.APACHE_COMBINED:
-      return async (
-        req: Request,
-        ctx: MiddlewareHandlerContext,
-      ): Promise<Response> => {
-        const now = options?.utcTime ? DateTime.utc() : DateTime.now();
-
-        const start = performance.now();
-        const res = await ctx.next();
-        const end = performance.now();
-        const duration = (end - start).toFixed(1);
-        let durationText = "";
-        if (includeDuration) {
-          res.headers.set("Server-Timing", `handler;dur=${duration}`);
-          durationText = `${duration}ms`;
-        }
-        const logParts = [
-          (ctx.remoteAddr as Deno.NetAddr).hostname,
-          await resolvers[ResolutionField.rfc931]?.(req, ctx, res),
-          await resolvers[ResolutionField.authuser]?.(req, ctx, res),
-          `[${now.toFormat("dd/MMM/yyyy:HH:mm:ss ZZZ")}]`,
-          `"${req.method} ${req.url}"`,
-          res.status,
-          await resolvers[ResolutionField.bytes]?.(req, ctx, res),
-        ]
-          .concat(combinedHeaders.slice(0, 2).map((header) => req.headers.has(header) ? `"${req.headers.get(header)}"` : "-"))
+      return async (req: Request, ctx: MiddlewareHandlerContext): Promise<Response> => {
+        const [durationText, res, commonLogParts] = await commonLogPartsResolver(req, ctx) as [string, Response, string[]];
+        const logParts = commonLogParts
+          .concat(combinedHeaders.slice(0, 2).map((header) => headerResolver(req, ctx, res, header)))
           .concat([durationText]);
         logger(logParts.join(" "));
-
         return res;
       };
     default:
